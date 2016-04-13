f3320d9ade5e6e874391e9e2ae416d6b70d7632c
HATSUMATSU <mail@hatsumatsu.de>
2016-04-13 09:53:26 +0200

    initial commit

---
+++ hm-network-auto-upload.php
@@ -0,0 +1,142 @@
+<?php
+
+/*
+Plugin Name: HM Network Auto Upload
+Version: 0.1
+Description: Automatically copies and attachs files uploaded to one site to all other sites in the network
+Plugin URI:
+Author: Martin Wecke, HATSUMATSU
+Author URI: http://hatsumatsu.de/
+*/
+
+
+class HMNetworkAutoUpload {
+    protected $mpl_api_cache;
+
+    public function __construct() {
+        // cache MPL API
+        add_action( 'inpsyde_mlp_loaded', array( $this, 'cache_mlp_api' ) );
+
+        add_filter( 'add_attachment', array( $this, 'copy_file' ), 10, 2 );
+    }
+
+
+    /**
+     * Cache MultillingualPress API
+     */
+    public function cache_mlp_api( $data ) {
+        $this->write_log( 'inpsyde_mlp_loaded' );
+
+        $this->mpl_api_cache = $data;   
+    }
+
+
+    /**
+     * Copy attachment to other sites of the network
+     *
+     * https://gist.github.com/hissy/7352933
+     * https://github.com/inpsyde/multilingual-press/issues/117
+     */
+    public function copy_file( $source_post_id ) {
+        $this->write_log( 'copy_file' );
+
+        $source_site_id = get_current_blog_id();
+        $source_parent_id = get_post_field( 'post_parent', $source_post_id, null );
+
+        $sites = wp_get_sites();
+
+        // each site... 
+        foreach( $sites as $site ) {
+            // ... that is not the source site
+            if( $site['blog_id'] != $source_site_id ) {
+                $src = wp_get_attachment_image_src( $source_post_id, 'full' )[0];
+
+                $file = get_attached_file( $source_post_id );
+                $filename = basename( $file );
+                $target_parent_id = 0;
+
+                // if file is attached to a post and MultilingualPress is present
+                if( $source_parent_id && function_exists( 'mlp_get_linked_elements' ) ) {
+                    $relations = mlp_get_linked_elements( $source_parent_id, '', $source_site_id );
+
+                    // if there is a connected post
+                    if( array_key_exists( $site['blog_id'], $relations ) ) {
+                        $target_parent_id = $relations[$site['blog_id']];
+                    }
+                }
+
+                // switch to other site
+                switch_to_blog( $site['blog_id'] );
+
+                // upload file
+                $upload_file = wp_upload_bits( $filename, null, file_get_contents( $file ) );
+                // file upload successful
+                if( !$upload_file['error'] ) {
+                    $wp_filetype = wp_check_filetype( $filename, null );
+                    $target = array(
+                        'post_mime_type' => $wp_filetype['type'],
+                        'post_parent' => $target_parent_id,
+                        'post_title' => preg_replace('/\.[^.]+$/', '', $filename ),
+                        'post_content' => '',
+                        'post_status' => 'inherit'
+                    );
+
+                    // remove filter to prevent infinite loop
+                    remove_filter( 'add_attachment', array( $this, 'copy_file' ) );
+                    // create attachment
+                    $target_post_id = wp_insert_attachment( $target, $upload_file['file'], $target_parent_id );
+                    // reintroduce filter
+                    add_filter( 'add_attachment', array( $this, 'copy_file' ), 10, 2 );
+                
+                    // attachment creation successful
+                    if( !is_wp_error( $target_post_id ) ) {
+                        require_once( ABSPATH . 'wp-admin/includes/image.php' );
+
+                        // create attachment meta data
+                        $target_data = wp_generate_attachment_metadata( $target_post_id, $upload_file['file'] );
+                        wp_update_attachment_metadata( $target_post_id,  $target_data );
+
+                        // link as translation if MultiligualPress is present
+                        
+                        if( $this->mpl_api_cache ) {            
+                            $this->write_log( 'API cache found' );
+
+                            $relations = $this->mpl_api_cache->get( 'content_relations' );
+
+                            $relations->set_relation(
+                                $source_site_id,
+                                $site['blog_id'],
+                                $source_post_id,
+                                $target_post_id
+                            );
+                        }
+
+                        $this->write_log( 'before add_action' );
+                    }
+                }
+            }
+
+            // switch back to current site
+            restore_current_blog();
+        }
+
+        return $source_post_id;
+    }
+
+
+    /**
+     * Write log if WP_DEBUG is active
+     * @param  string|array $log 
+     */
+    public function write_log( $log )  {
+        if( true === WP_DEBUG ) {
+            if( is_array( $log ) || is_object( $log ) ) {
+                error_log( 'hmnau: ' . print_r( $log, true ) . "\n", 3, trailingslashit( ABSPATH ) . 'wp-content/debuglog.log' );
+            } else {
+                error_log( 'hmnau: ' . $log . "\n", 3, trailingslashit( ABSPATH ) . 'wp-content/debuglog.log' );
+            }
+        }
+    }
+}
+
+new HMNetworkAutoUpload();
